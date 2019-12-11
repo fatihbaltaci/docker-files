078b17b5ccf687c88343d40bb55626618d2ebae8
Fatih Baltacı <baltacifatih14@gmail.com>
2019-12-11 15:04:44 +0300

    bash script deleted

--- docker-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#changed_docker_files=( $(git diff-tree --no-commit-id --name-only -r $GITHUB_SHA | grep Dockerfile) )
-changed_docker_files=( $(git diff-tree --no-commit-id --name-only -r 5d06329a59f8dce74732b73271b0db7dbde102ba) )
-
-echo $changed_docker_files
