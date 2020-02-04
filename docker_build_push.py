bff3562914d9ff2cdb08b78b189b5b112e0d6a2a
Fatih Baltacı <baltacifatih14@gmail.com>
2020-02-05 01:08:08 +0300

    auto docker build push code updated for github actions

--- docker_build_push.py
+++ docker_build_push.py
@@ -1,11 +1,11 @@
 # Python 3.7
 
+import os
 import subprocess
 from pathlib import Path
 
 
 def prepare_docker_build(file_path, hub_username):
-    print(file_path)
     file_dir = file_path.split("/Dockerfile")[0]
     splitted_dir = file_dir.split("/")
     name = splitted_dir[0]  # libtorch
@@ -18,15 +18,20 @@ def prepare_docker_build(file_path, hub_username):
 def run_command(command):
     try:
         process = subprocess.run(
-            command, shell=True, capture_output=True, check=True)
-        return process.stdout.decode("utf-8")
+            command, shell=True, capture_output=True, check=True, universal_newlines=True)
+        return process.stdout
     except subprocess.CalledProcessError as error:
-        print("Exception Occured: " + str(error))
+        return f"Exception Occured: {str(error)}"
+
+def docker_build(command):
+    return_code = os.system(command)
+    if return_code != 0:
+        print("There is a problem !!!")
 
 
 if __name__ == "__main__":
 
-    COMMAND_CHANGED_DOCKERFILES = "git diff-tree --no-commit-id --name-only -r f553255007e46af1d7a4ba1833b2dbc30e2c7563"
+    COMMAND_CHANGED_DOCKERFILES = "git diff-tree --no-commit-id --name-only -r 5b1705b68444794feb7cf41d531c85b5a6c5e027"
     DOCKERHUB_USERNAME = "fatihbaltaci"
 
     process_git = run_command(COMMAND_CHANGED_DOCKERFILES)
@@ -37,12 +42,11 @@ if __name__ == "__main__":
             continue
         print("Changed Dockerfile: " + file_path)
 
-        file_path = "python_3.6/Dockerfile"
-
         dockerfile_dir, image_tag = prepare_docker_build(file_path=file_path, hub_username=DOCKERHUB_USERNAME)
 
-        COMMAND_BUILD_DOCKER_FILE = "cd " + dockerfile_dir + " && docker build . --tag " + \
-            image_tag
+        docker_file_path = Path(dockerfile_dir) / "Dockerfile"
+
+        COMMAND_BUILD_DOCKER_FILE = f"docker build --tag {image_tag} -f {docker_file_path} . "
+        
         print(COMMAND_BUILD_DOCKER_FILE)
-        output = run_command(COMMAND_BUILD_DOCKER_FILE)
-        print(output)
+        docker_build(COMMAND_BUILD_DOCKER_FILE)
